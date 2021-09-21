b48cf0be9feed8118877f8e30a9ae09e68a16ddc
Anthony <tonierobie@gmail.com>
2021-09-21 20:18:25 +0300

    View class

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -1,9 +1,13 @@
-<?php
+<?php 
 
 namespace App\Controllers;
 
 use Core\Controller;
 
 class BlogController extends Controller {
+
+    public function indexAction(){
+        $this->view->render();
+    }
     
 }
--- core/Router.php
+++ core/Router.php
@@ -7,7 +7,29 @@ use App\Controllers\BlogController;
 class Router {
 
     public static function route($url) {
-        $controller = new BlogController('Blog', 'indexAction');
+        $urlParts = explode("/", $url);
+        
+        // set controller
+        $controller = !empty($urlParts[0])? $urlParts[0] : Config::get('default_controller');
+        $controllerName = $controller;
+        $controller = '\App\Controllers\\' . ucwords($controller) . 'Controller';
 
+        // set action 
+        array_shift($urlParts);
+        $action = !empty($urlParts[0])? $urlParts[0] : 'index';
+        $actionName = $action;
+        $action .= "Action";
+        array_shift($urlParts);
+
+
+        if(!class_exists($controller)){
+            throw new \Exception("The controller \"{$controllerName}\" does not exist.");
+        }
+        $controllerClass = new $controller($controllerName, $actionName);
+
+        if(!method_exists($controllerClass, $action)) {
+            throw new \Exception("The method \"{$action}\" does not exist on the \"{$controller}\" controller.");
+        }
+        call_user_func_array([$controllerClass, $action], $urlParts);
     }
 }
