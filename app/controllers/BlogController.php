98e5ad616999b31cfdee47ad8d62c65c2d316316
Anthony <tonierobie@gmail.com>
2021-09-24 17:13:36 +0300

    insert and update method

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -8,6 +8,9 @@ class BlogController extends Controller {
 
     public function indexAction(){
         $db = DB::getInstance();
+        $sql = "INSERT INTO articles (`title`, `body`) VALUES (:title, :body)";
+        $bind = ['title'=>'new article 1', 'body'=> 'article body 1'];
+        $db->execute($sql, $bind);
         $this->view->setSiteTitle('Blog');
         $this->view->render();
     }
--- core/DB.php
+++ core/DB.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 namespace Core;
 
 use \PDO;
@@ -7,6 +7,7 @@ use Core\{Config, H};
 
 class DB {
     protected $_dbh, $_results, $_lastInsertId, $_rowCount = 0, $_fetchType = PDO::FETCH_OBJ, $_class, $_error = false;
+    protected $_stmt;
     protected static $_db;
 
     public function __construct() {
@@ -31,5 +32,77 @@ class DB {
             self::$_db = new self();
         }
         return self::$_db;
+    }
+
+    public function execute($sql, $bind=[]){
+        $this->_results = null;
+        $this->_lastInsertId = null;
+        $this->_error = false;
+        $this->_stmt = $this->_dbh->prepare($sql);
+        if(!$this->_stmt->execute($bind)) {
+            $this->_error = true;
+        } else {
+            $this->_lastInsertId = $this->_dbh->lastInsertId();
+        }
+
+        return $this;
+    }
+
+    public function query($sql, $bind=[]) {
+        $this->execute($sql, $bind);
+        if(!$this->_error) {
+            $this->_rowCount = $this->_stmt->rowCount();
+            $this->_results = $this->_stmt->fetchAll($this->_fetchType);
+        }
+        return $this;
+    }
+
+    public function insert($table, $values) {
+        $fields = [];
+        $binds = [];
+        foreach($values as $key => $value) {
+            $fields[] = $key;
+            $binds[] = ":{$key}";
+        }
+        $fieldStr = implode('`, `', $fields);
+        $bindStr = implode(', ', $binds);
+        $sql = "INSERT INTO {$table} (`{$fieldStr}`) VALUES ({$bindStr})";
+        $this->execute($sql, $values);
+        return !$this->_error;
+    }
+
+    public function update($table, $values, $conditions) {
+        $binds = [];
+        $valueStr = "";
+        foreach($values as $field => $value) {
+            $valueStr .= ", `{$field}` = :{$field}";
+            $binds[$field] = $value;
+        }
+        $valueStr = ltrim($valueStr, ', ');
+        $sql = "UPDATE {$table} SET {$valueStr}";
+
+        if(!empty($conditions)) {
+            $conditionStr = " WHERE ";
+            foreach($conditions as $field => $value) {
+                $conditionStr .= "`{$field}` = :cond{$field} AND ";
+                $binds['cond'.$field] = $value;
+            }
+            $conditionStr = rtrim($conditionStr, ' AND ');
+            $sql .= $conditionStr;
+        }
+        $this->execute($sql, $binds);
+        return !$this->_error;
+    }
+
+    public function results(){
+        return $this->_results;
+    }
+
+    public function count() {
+        return $this->_rowCount;
+    }
+
+    public function lastInsertId(){
+        return $this->_lastInsertId;
     }
 }
